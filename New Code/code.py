b4ee73ce46274b0b5ad1ec71cc934c69e0fc11d1
gzny1998 <63383809+gzny1998@users.noreply.github.com>
2020-04-10 02:58:24 -0400

    Update code.py

--- New Code/code.py
+++ New Code/code.py
@@ -19,6 +19,37 @@ import random
 ###############################################
 ############## For Classes ONLY ###############
 
+class Buoy:
+    def __init__(self, lat_coord, long_coord, lat_speed, long_speed):
+        self.lat_coord = lat_coord
+        self.long_coord = long_coord
+        self.lat_speed = lat_speed
+        self.long_speed = long_speed
+
+    def set_lat_coord(self, coord):
+        self.lat_coord += coord
+
+    def get_lat_coord(self):
+        return self.lat_coord
+
+    def set_long_coord(self, coord):
+        self.long_coord += coord
+
+    def get_long_coord(self):
+        return self.long_coord
+
+    def set_lat_speed(self, speed):
+        self.lat_speed = speed
+
+    def get_lat_speed(self):
+        return self.lat_speed
+
+    def set_long_speed(self, speed):
+        self.long_speed = speed
+
+    def get_long_speed(self):
+        return self.long_speed
+
 # Sets up a class responsible for controlling each individual thruster's speed
 class Thruster:
     def __init__(self, name, speed):
@@ -37,14 +68,26 @@ class Thruster:
 ##############################################
 ################# Variables ##################
 # Defines the maximum speed that the thrusters can go at and what the STOP signal is
-MAX_FORWARD_SPEED   = 190
-NEUTRAL             = 150
-MAX_BACKWARD_SPEED  = 110
+# Actual hardware has max forward pulsewidth of 1900us and reverse of 1100us and a stop of 1500us
+# In simulation, we map 1900us to 4 and 1100us to -4.
+MAX_FORWARD_SPEED   = 4
+NEUTRAL             = 0
+MAX_BACKWARD_SPEED  = -4
 
 # Splits up the 4 thrusters' adjustment speeds into 2 sets: LONG (NS) and LAT (WE)
 # ADJUSTMENT is added to NEUTRAL, which is what causes the thrusters to spin either foward or backwards
-LONG_ADJUSTMENT     = 0
-LAT_ADJUSTMENT      = 0
+long_adjustment     = 0
+lat_adjustment      = 0
+
+# Stores the UUV's axis' signs. NORTH/EAST is positive. WEST/SOUTH is negative.
+# These values only changes once sign_int's value is different than sign_current
+# These values are set prior to any speed adjustment
+long_sign_int   = 0
+lat_sign_int    = 0
+
+# These values are set as the system is running and is compared against sign_int
+long_sign_current   = 0
+lat_sign_current    = 0
 
 # Initializes adjustment_time, which is used to delay changes in thruster speeds
 long_adjust_time    = 0
@@ -54,6 +97,9 @@ lat_adjust_time     = 0
 long_scale          = 1
 lat_scale           = 1
 
+# Previous Location
+long_old = 0
+lat_old = 0
 ##############################################
 ################## Objects ###################
 
@@ -71,9 +117,17 @@ PINS = [NORTH, SOUTH, WEST, EAST]
 ##### Simulation Functions and Variables #####
 
 # Functions
-def random_change():
-    # generates and returns a value between -1 to 1, step value of 0.01
-    return random.randrange(-100, 100, 1) / 100
+def random_change(step_value):
+    # generates and returns a value between |100/step_value| with a step of 1/step_value
+    return random.randrange(-100, 100, 1) / step_value
+
+'''def update_position():
+    adjusted_lat_speed = (long_scale * 10) - wave_lat_current
+    adjusted_long_speed = (lat_scale * 10) - wave_long_current
+
+    print(adjusted_lat_speed)
+    print(adjusted_long_speed)'''
+
 
 # Variables
 # Sets up the home location to 0, 0 -- corresponds to a XY coordinate system
@@ -81,8 +135,19 @@ HOME_LAT = 0
 HOME_LONG = 0
 
 # Calls random_change for each one, generating a random LAT/LONG to start off
-LAT = random_change()
-LONG = random_change()
+LAT = random_change(5)
+LONG = random_change(5)
+
+# Wave Current (from -4 to +4 m/s)
+wave_lat_current    = random_change(33)
+wave_long_current   = random_change(33)
+
+# UUV speed
+UUV_long_speed      = 0
+UUV_lat_speed       = 0
+
+# Initializes the Buoy object
+UUV = Buoy(LAT, LONG, wave_lat_current, wave_long_current)
 
 ##############################################
 ########## Functions and Main Loop ###########
@@ -106,8 +171,8 @@ def gps_fix():
 # Remove for hardware integration
 def gps_update():
     global LONG, LAT
-    LONG = random_change()
-    LAT = random_change()
+    LONG = random_change(5)
+    LAT = random_change(5)
     return
 
 # Stops the thrusters given by the array PINS
@@ -118,33 +183,30 @@ def stop_thrusters(PINS):
             long_scale = 1
         if (PIN == EAST) or (PIN == WEST):
             lat_scale = 1
-        print("Stopping Thruster {}".format(PIN.get_name()))
+        '''print("Stopping Thruster {}".format(PIN.get_name()))'''
     time.sleep(0.01)
 
 # Sets the relevant thrusters to a speed defined by DUTYCYCLE
 def run_thrusters(PINS, DUTYCYCLE):
     for PIN in PINS:
         PIN.set_speed(DUTYCYCLE)
-        print("Setting thruster {0} to {1}".format(PIN.get_name(), DUTYCYCLE))
-    print("")
+        '''print("Setting thruster {0} to {1}".format(PIN.get_name(), DUTYCYCLE))
+    print("")'''
     time.sleep(0.1)
 
 # Tolerance compares the magnitudes of the LONG/LAT values with a fixed value,
 # which corresponds to the +- 10ft specification
-def tolerance(LONG, LAT):
-    tolerance = 0.01
-    tolerance_list = [False] * 2
+def tolerance(coord):
+    tolerance = 3
 
-    if abs(LONG) > 0.01:
-        tolerance_list[0] = True
-    if abs(LAT) > 0.01:
-        tolerance_list[1] = True
-
-    message = "Tolerance Check\n{0:<20s}{1:<.4f}\n{2:<20s}{3:<}\n{4:<20s}{5:<}\n".format(
+    if abs(coord) > tolerance:
+        return True
+    else:
+        return False
+    '''message = "Tolerance Check\n{0:<20s}{1:<.4f}\n{2:<20s}{3:<}\n{4:<20s}{5:<}\n".format(
         "Tolerance Value:", tolerance, "Longitude:", str(tolerance_list[0]), "Latitude:", str(tolerance_list[1])
     )
-    print(message)
-    return tolerance_list
+    print(message)'''
 
 # Can be optimized
 # Takes in thrusters that need to be adjusted
@@ -152,27 +214,38 @@ def tolerance(LONG, LAT):
 # scale = how much should the adjustment to speed be
 # coord defines if the change is either a positive or negative
 
-def adjust_speed(PINS, t, scale, coord):
-    adjustment = 10 * scale
-
+def adjust_speed(PINS, t, scale, coord, speed):
+    global lat_old, long_old
+    adjustment = scale
     if (time.monotonic() - t > 1) or scale == 1:
-        if (10 * scale) >= 41:
+        if scale >= 4000:
             scale = scale
+        elif not tolerance(coord):
+            return [t, scale, adjustment]
+        elif abs(speed) >= 1:
+            scale *= 2
         else:
             scale += 1
-            adjustment = 10 * (scale - 1)
-            t = time.monotonic()
 
-            if coord > 0:
-                run_thrusters(PINS, NEUTRAL - adjustment)
-            else:
-                run_thrusters(PINS, NEUTRAL + adjustment)
+        adjustment = 4 * math.exp(scale/10 - 5)/(math.exp(scale/10 - 5) + 1)
+
+        t = time.monotonic()
+
+        if coord > 0:
+            run_thrusters(PINS, NEUTRAL - adjustment)
+        else:
+            run_thrusters(PINS, NEUTRAL + adjustment)
 
         return [t, scale, adjustment]
 
     else:
         return [t, scale, adjustment]
 
+def coord_sign(coord):
+    if coord >= 0:
+        return 1
+    else:
+        return -1
 
 def main():
     # Comment out/remove once simulated movement has been implemented
@@ -186,69 +259,99 @@ def main():
     gps_update()
 
     # Compares change from HOME location [0, 0]
-    long_change = HOME_LONG - LONG
-    lat_change = HOME_LAT - LAT
+    lat_sign_int = coord_sign(LAT)
+    long_sign_int = coord_sign(LONG)
+
+    UUV.set_lat_coord(LAT)
+    UUV.set_long_coord(LONG)
+    UUV.set_lat_speed(wave_lat_current)
+    UUV.set_long_speed(wave_long_current)
 
     # Main Loop
     while(1):
-        global adjustment_time, LAT_ADJUSTMENT, LONG_ADJUSTMENT, lat_adjust_time, long_adjust_time
-        global lat_scale, long_scale
+        global adjustment_time, lat_adjustment, long_adjustment, lat_adjust_time, long_adjust_time
+        global lat_scale, long_scale, long_sign_current, lat_sign_current
 
         last_runtime = time.monotonic()
         try:
             current_time = time.monotonic()
 
-            # Update and compare coordinates
-            #gps_update()
-            long_change = HOME_LONG - LONG
-            lat_change = HOME_LAT - LAT
+            # UCompare coordinates
+            long_sign_current = coord_sign(UUV.get_long_coord())
+            lat_sign_current = coord_sign(UUV.get_lat_coord())
 
             #Delay update for a maximum of 0.1s
-            time.sleep(5 - (current_time - last_runtime))
+            time.sleep(1 - (current_time - last_runtime))
+            wait = time.monotonic() - last_runtime
+
+            UUV.set_long_speed(NORTH.get_speed() - wave_long_current)
+            UUV.set_lat_speed(WEST.get_speed() - wave_lat_current)
+            UUV.set_long_coord(UUV.get_long_speed() * wait)
+            UUV.set_lat_coord(UUV.get_lat_speed() * wait)
+
+            print("{0:12s}{1:>5f}\n{2:12s}{3:>5f}\n".format("Lat Post:", UUV.get_lat_coord(),
+                                                          "Long Post:", UUV.get_long_coord()))
+
+            #print(wave_lat_current, wave_long_current)
+
+            '''print("{0:12s}{1:>5f}\n{2:12s}{3:>5f}\n".format("Long Speed:", UUV.get_long_speed(),
+                                                          "Lat Speed:", UUV.get_lat_speed()))'''
 
             last_runtime = current_time
 
-            # Calls tolerance to see if out of bounds
-            case = tolerance(long_change, lat_change)
-
             # Changes set location -- remove/comment out if necessary
-            print(time.monotonic() - time_since_GPS_change)
-            if time.monotonic() - time_since_GPS_change >= 20:
+            #print(time.monotonic() - time_since_GPS_change)
+            '''if time.monotonic() - time_since_GPS_change >= 20:
                 time_since_GPS_change = time.monotonic()
                 gps_update()
                 long_scale = 0
                 lat_scale = 0
                 print(long_change)
-                print(lat_change)
+                print(lat_change)'''
 
             # Responsible for LONG
             # Currently barebones
-            if case[0]:
+            if tolerance(UUV.get_long_coord()):
                 if long_adjust_time == 0:
                     long_adjust_time = time.monotonic()
-                change = adjust_speed([NORTH, SOUTH], long_adjust_time, long_scale, long_change)
-                long_adjust_time = change[0]
-                long_scale = change[1]
-                LONG_ADJUSTMENT = change[2]
+
+                change = adjust_speed([NORTH, SOUTH], long_adjust_time, long_scale, UUV.get_long_coord(), UUV.get_long_speed())
+
+                long_adjust_time    = change[0]
+                long_scale          = change[1]
+                long_adjustment     = change[2]
+
+                if not(long_sign_int == long_sign_current):
+                    long_sign_int = coord_sign(UUV.get_long_coord())
+                    long_scale -= 1
 
             else:
                 stop_thrusters([NORTH, SOUTH])
 
             # Responsible for LAT
             # Currently barebones
-            if case[1]:
+            if tolerance(UUV.get_lat_coord()):
                 if lat_adjust_time == 0:
                     lat_adjust_time = time.monotonic()
-                adjust_speed([WEST, EAST], lat_adjust_time, lat_scale, lat_change)
-                lat_adjust_time = change[0]
-                lat_scale = change[1]
-                LAT_ADJUSTMENT = change[2]
+
+                change = adjust_speed([WEST, EAST], lat_adjust_time, lat_scale, UUV.get_lat_coord(), UUV.get_lat_speed())
+
+                lat_adjust_time     = change[0]
+                lat_scale           = change[1]
+                lat_adjustment      = change[2]
+
+                if not(lat_sign_int == lat_sign_current):
+                    lat_sign_int = coord_sign(UUV.get_lat_coord())
+                    lat_scale -= 1
 
             else:
                 stop_thrusters([WEST, EAST])
 
         except KeyboardInterrupt:
-            break
+            # Press CTRL+C to activate. Updates GPS position
+            gps_update()
+            print(LONG, LAT)
+            print(wave_long_current, wave_lat_current)
 
 if __name__ == '__main__':
     main()
